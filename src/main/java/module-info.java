1daa83c9006c51e7411510c624e603cb282f9712
Jin Kwon <onacit@gmail.com>
2024-04-17 13:28:55 +0900

    wip

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,3 +1,11 @@
+/**
+ * Defines classes for accessing bound information from <a href="https://www.kftc.or.kr">긍융결재원 (KFTC)</a> <a
+ * href="https://www.kftc.or.kr/kftc/data/EgovBankListMove.do">금융회사 코드</a> 정보.
+ *
+ * @author Jin Kwon &lt;onacit_at_gmail.com&gt;
+ * @see <a href="https://www.kftc.or.kr">긍융결재원 (KFTC)</a>
+ * @see <a href="https://www.kftc.or.kr/kftc/data/EgovBankListMove.do">금융회사 코드</a>.
+ */
 module com.github.jinahya.kftc.financial.institution.codes {
     exports com.github.jinahya.kftc.financial.institution.codes;
 }
