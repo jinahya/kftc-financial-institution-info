3ab458fc944802fc2b7368c74696c8144566a00f
Jin Kwon <onacit@gmail.com>
2024-05-17 13:28:55 +0900

    wip

---
+++ src/main/java/com/github/jinahya/kftc/financial/institution/info/_Column.java
@@ -0,0 +1,13 @@
+package com.github.jinahya.kftc.financial.institution.info;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD})
+public @interface _Column {
+
+    String value();
+}
