bf04d243fc025c4880b59b16dc12d32bb4b713ec
Jin Kwon <onacit@gmail.com>
2024-06-17 12:48:21 +0900

    Add an anchor for GitHub

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -25,6 +25,7 @@
  * @author Jin Kwon &lt;onacit_at_gmail.com&gt;
  * @see <a href="https://www.kftc.or.kr">긍융결재원(KFTC)</a>
  * @see <a href="https://www.kftc.or.kr/kftc/data/EgovBankList.do">금융회사코드 조회</a>
+ * @see <a href="https://github.com/jinahya/kftc-financial-institution-info">GitHub</a>
  */
 module com.github.jinahya.kftc.financial.institution.info {
     requires transitive java.sql;
