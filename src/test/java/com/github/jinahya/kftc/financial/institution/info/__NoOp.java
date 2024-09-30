612e674a2eaf4cb37035444df75b8fac1f537f3a
Jin Kwon <onacit@gmail.com>
2024-09-30 16:31:15 +0900

    wip

---
+++ src/test/java/com/github/jinahya/kftc/financial/institution/info/__NoOp.java
@@ -0,0 +1,13 @@
+package com.github.jinahya.kftc.financial.institution.info;
+
+/**
+ * Just a no-op class.
+ *
+ * @author Jin Kwon &lt;onacit_at_gmail.com&gt;
+ */
+public final class __NoOp {
+
+    private __NoOp() {
+        throw new AssertionError("instantiation is not allowed");
+    }
+}
